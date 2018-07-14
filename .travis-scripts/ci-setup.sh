a028fe149faaf58b37c0278beeb5ec3d45c30a70
coaxial <hi@64b.it>
2018-07-14 19:50:06 -0400

    Cleanup

--- .travis-scripts/ci-setup.sh
+++ .travis-scripts/ci-setup.sh
@@ -1,9 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 sudo apt-get update
-# sudo apt-get install dnsmasq -y
+# xenial ships LXD 2 LTS, backports ships 3 LTS
 sudo apt-get upgrade -t xenial-backports -y lxd lxd-client
+
 sudo lxd --version
+# Wait for the socket to be ready
 sudo lxd waitready
 sudo lxd init --auto
+
 pip install ansible
 pip install molecule
