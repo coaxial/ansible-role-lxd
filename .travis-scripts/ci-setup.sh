78dcf6e5c12d1e2f51b54de4810d66cf3a8fe6cc
coaxial <hi@64b.it>
2018-07-13 21:24:37 -0400

    wip debug

--- .travis-scripts/ci-setup.sh
+++ .travis-scripts/ci-setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Mostly ripped off from https://github.com/lxdock/lxdock/blob/master/scripts/ci-base-setup.sh
 
-set -xe
-
 sudo -E apt-get update
 sudo -E apt-get purge -y lxd lxd-client
 sudo -E apt-get install -y snapd
@@ -23,8 +21,14 @@ sudo usermod -a -G lxd travis
 # lxd init now sets up a bridge so we no longer need to
 sudo lxd init --auto
 
+lxc launch ubuntu:18.04 ci -c security.nesting=true
+lxc exec ci -- apt install git python -yq
+lxc exec ci -- pip install ansible
+lxc exec ci -- pip install molecule
+lxc exec ci -- git clone --depth=50 https://github.com/coaxial/ansible-role-lxd.git coaxial/ansible-role-lxd
+
 # sudo virtualenv $HOME/travis-venv
 # source $HOME/travis-venv/bin/activate
-sudo chmod 666 /var/snap/lxd/common/lxd/unix.socket
-pip install molecule
-pip install ansible
+# sudo chmod 666 /var/snap/lxd/common/lxd/unix.socket
+# pip install molecule
+# pip install ansible
