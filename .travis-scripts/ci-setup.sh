4ca0467ae8a1194e7d894c45c370a597f0ef91ac
coaxial <hi@64b.it>
2018-07-14 19:19:48 -0400

    wip debug

--- .travis-scripts/ci-setup.sh
+++ .travis-scripts/ci-setup.sh
@@ -57,4 +57,4 @@ pip install molecule
 env
 sudo env
 sudo -E env
-sudo bash -c 'source source ~/virtualenv/python2.7/bin/activate && env'
+sudo bash -c 'source ~/virtualenv/python2.7/bin/activate && env'
